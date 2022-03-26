3685dc4f95d262fa249bcf9b03d1785a478fc80b
tomquinn04 <72301139+tomquinn04@users.noreply.github.com>
2022-03-26 12:38:53 +0000

    setup

--- SignalPOS.dpr
+++ SignalPOS.dpr
@@ -3,12 +3,14 @@ program SignalPOS;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  splash in 'splash.pas' {Form1};
+  splash in 'forms\splash.pas' {Form1},
+  datamodule in 'data\datamodule.pas' {DataModule1: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
---
+++ data/datamodule.dfm
@@ -0,0 +1,5 @@
+object DataModule1: TDataModule1
+  Height = 251
+  Width = 486
+  PixelsPerInch = 120
+end
